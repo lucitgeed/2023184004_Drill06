edcab2e5f4680ebc30573f578caf57db218a20fc
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-04 15:07:37 +0900

    뼈대잡기(랜덤으로 움직이는 손, 따라가는 캐릭 함수 선언))

---
+++ 2023184004_Drill06_follow_hand.py
@@ -0,0 +1,19 @@
+from pico2d import*
+
+def hand_to_rand():
+    print("handrand")
+    pass
+
+
+def Follow_hand():
+    print("follow")
+    pass
+
+
+
+while True:
+
+    hand_to_rand()
+
+    Follow_hand()
+
