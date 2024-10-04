fa789db7b01459fbc18f3fc8187c2854a1933d09
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-04 17:39:02 +0900

    follow_hand 대충 구현

--- 2023184004_Drill06_follow_hand.py
+++ 2023184004_Drill06_follow_hand.py
@@ -20,22 +20,43 @@ def hand_to_rand(x, y):
 #
 
 
-def Follow_hand():
-
- #   back.draw(600, 500, 1200, 1000)
+#
+def Follow_hand(x, y):
     print("follow")
-    char.clip_draw(0, 300, 100, 100, 800//2, 600//2)
 
-    #캐릭터(정자세)를 먼저 그리고
+    xnow = 800//2
+    ynow = 600//2
+    frame = 0
 
     #캐릭터 애니메이션 프레임구현(방향전환)
-    #now는 현재 위치
+    if x >= xnow:
+        for i in range(0, 100+1, 4):
+            t = i / 100
+            xline = (1 - t) * xnow + t * x
+            yline = (1 - t) * ynow + t * y
+
+            clear_canvas()
+            char.clip_draw(frame * 100, 100, 100, 100, int(xline), int(yline), 100, 100)
+            frame = frame + 1
+            update_canvas()
+            delay(0.1)
+
+        char.clip_draw(0, 300, 100, 100, x, y)
 
 
-    #화살표를 찾아서 따라감
-    #직선으로
-    #따라가는 과정이 보임
-    update_canvas()
+    elif x < xnow:
+        for i in range(0, 100+1, 4):
+            t = i / 100
+            xline = (1 - t) * xnow + t * x
+            yline = (1 - t) * ynow + t * y
+
+            clear_canvas()
+            char.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', int(xline), int(yline), 100, 100)
+            frame = frame + 1
+            update_canvas()
+            delay(0.1)
+
+        char.clip_composite_draw(0, 300, 100, 100, 0, 'h', x, y)
 
     pass
 
@@ -44,10 +65,10 @@ def Follow_hand():
 
 #char.draw(400, 300)
 while True:
-#    x = random.randint(25,785)
-#    y = random.randint(26,584)
+    x = random.randint(25,785)
+    y = random.randint(26,584)
 
  #   hand_to_rand(x, y)
 
-    Follow_hand()
+    Follow_hand(x, y)
 
