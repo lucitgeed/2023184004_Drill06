79432e08e6f10deea28650fea80db0b423d8240c
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-04 21:39:04 +0900

    쭉(400,300)이 아닌 hand의 마지막 위치에서 출발하게 구현

--- 2023184004_Drill06_follow_hand.py
+++ 2023184004_Drill06_follow_hand.py
@@ -8,40 +8,23 @@ char = load_image('animation_sheet.png')
 back = load_image('TUK_GROUND.png')
 
 
-#
-def hand_to_rand(x, y):
-    back.draw(600,500,1200,1000)
-    hand.draw(x, y)
-#
-
-
-
-
-
-
-
-
-
-
 
 #
-def Follow_hand(x, y):
+def Follow_hand(x, y, lastx, lasty):
 
-    xnow = 800//2
-    ynow = 600//2
     frame = 0
-
     for i in range(0, 100+1, 2):
         t = i / 100
-        xline = (1 - t) * xnow + t * x
-        yline = (1 - t) * ynow + t * y
+        xline = (1 - t) * lastx + t * x
+        yline = (1 - t) * lasty + t * y
 
         clear_canvas()
+        back.draw(600, 500, 1200, 1000)
+        hand.draw(x, y)
 
-        hand_to_rand(x, y)
-        if x >= xnow:
+        if x >= lastx:
             char.clip_draw(frame * 100, 100, 100, 100, int(xline), int(yline), 100, 100)
-        elif x < xnow:
+        elif x < lastx:
             char.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', int(xline), int(yline), 100, 100)
         frame = (frame + 1) % 8
         delay(0.05)
@@ -50,12 +33,17 @@ def Follow_hand(x, y):
 #
 
 
+xlast = 800 // 2
+ylast = 600 // 2
+char.clip_draw(0, 300, 100, 100, 400, 300, 100, 100)
+delay(0.1)
 
-char.clip_draw(0, 300, 100, 100, 400, 300)
 while True:
     x = random.randint(25,785)
     y = random.randint(26,584)
 
+    Follow_hand(x, y, xlast, ylast)
 
-    Follow_hand(x, y)
+    xlast = x
+    ylast = y
 
