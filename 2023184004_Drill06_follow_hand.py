598bdf32c85a9d0a923715694aa1e46dfa4760ab
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-04 21:02:18 +0900

    코드 일부 정리

--- 2023184004_Drill06_follow_hand.py
+++ 2023184004_Drill06_follow_hand.py
@@ -26,59 +26,36 @@ def hand_to_rand(x, y):
 
 #
 def Follow_hand(x, y):
-    print("follow")
 
     xnow = 800//2
     ynow = 600//2
     frame = 0
 
-
-
-    for i in range(0, 100+1):
+    for i in range(0, 100+1, 2):
         t = i / 100
         xline = (1 - t) * xnow + t * x
         yline = (1 - t) * ynow + t * y
-        print("thisis t, linex liney")
 
-        print(t)
-        print(xline)
-        print(yline)
-
-        #clear_canvas()
+        clear_canvas()
 
         hand_to_rand(x, y)
-        print("draw hand")
-
         if x >= xnow:
             char.clip_draw(frame * 100, 100, 100, 100, int(xline), int(yline), 100, 100)
-            print("clipdraw rignt")
         elif x < xnow:
             char.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', int(xline), int(yline), 100, 100)
-            print("clipdraw left")
-
-
         frame = (frame + 1) % 8
-        print("frame +  1")
-
-        delay(0.1)
+        delay(0.05)
 
         update_canvas()
-        print("update canvas")
-
-     #   char.clip_draw(0, 300, 100, 100, x, y)
-    #char.clip_composite_draw(0, 300, 100, 100, 0, 'h', x, y)
-    #    update_canvas()
+#
 
 
 
-
-
-#char.draw(400, 300)
+char.clip_draw(0, 300, 100, 100, 400, 300)
 while True:
     x = random.randint(25,785)
     y = random.randint(26,584)
 
- #   hand_to_rand(x, y)
 
     Follow_hand(x, y)
 
