c19638b05d199a0b71d3841dfd863a2dd6b081b5
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-04 15:28:09 +0900

    hand_to_rand 뼈대잡고 작동하는지 체크

--- 2023184004_Drill06_follow_hand.py
+++ 2023184004_Drill06_follow_hand.py
@@ -1,7 +1,23 @@
 from pico2d import*
 
+open_canvas()
+hand = load_image('hand_arrow')
+char = load_image("animation_sheet")
+back = load_image('TUK_GROUND')
+
+
 def hand_to_rand():
     print("handrand")
+    clear_canvas()
+    back.draw(0,0)
+
+    char.draw(400, 300)
+    delay(0.1)
+
+
+    #손은 이동과정이 보이지 않음.
+    #화면 내에서 랜덤위치로 이동하고
+    #!!기다림!!
     pass
 
 
@@ -15,5 +31,5 @@ while True:
 
     hand_to_rand()
 
-    Follow_hand()
+ #   Follow_hand()
 
